b54d12059efb190b3d7fc2c336e056497fdcd9af
David.Morison <david.morison@moneysavingexpert.com>
2020-11-27 22:05:20 +0000

    main page header image hacking

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import './App.css';
 import Head from "./Head";
 import WineCard from "./WineCard";
 import Detail from "./Detail";
-import { getData } from '../utils/API';
-// import { mockData } from '../utils/mockData';
+// import { getData } from '../utils/API';
+import { mockData } from '../utils/mockData';
+import HeaderImage from "../assets/images/wine-grapes.jpg";
 
 const filterParams = {
 	Search: "",
@@ -49,20 +50,20 @@ const App = () => {
 	};
 
 	const getWines = async () => {
-		// let data = mockData;
-		// console.log(data);
-		// setInitWines(data);
-		// setWines(data);
-		try {
-			const { data } = await getData();
-			console.log(data.values);
-			let responseData = formatData(data.values);
-			console.log(responseData);
-			setInitWines(responseData);
-			setWines(responseData);
-		} catch (err) {
-			console.log(err);
-		}
+		let data = mockData;
+		console.log(data);
+		setInitWines(data);
+		setWines(data);
+		// try {
+		// 	const { data } = await getData();
+		// 	console.log(data.values);
+		// 	let responseData = formatData(data.values);
+		// 	console.log(responseData);
+		// 	setInitWines(responseData);
+		// 	setWines(responseData);
+		// } catch (err) {
+		// 	console.log(err);
+		// }
 	};
 
   return (
@@ -73,6 +74,14 @@ const App = () => {
 				filters={filters}
 				setFilters={setFilters}
 			/>
+			<Row>
+				<Col>
+					{/* <div className="header-wrapper">
+						<div className="header-image"></div>
+					</div> */}
+					{/* <img className="header-image" src={HeaderImage} alt="grape vines" /> */}
+				</Col>
+			</Row>
 			<Container>
 				<Row>
 					{wines.map((wine, i) => {
